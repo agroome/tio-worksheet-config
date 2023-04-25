9d2ff88f3c8f330aba2303159028a107f74f6f57
Andy Groome <groome.andy@gmail.com>
2023-04-24 21:38:02 -0400

    updated package_dir

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='tio-config',
     version='0.1.0',
-    packages=find_packages(where='tio-config'),
-    include_package_data=True,
     package_dir={"": "tio-config"},
 
     install_requires=[
