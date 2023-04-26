b31d48599a54749e447495b4e862443f798f2e47
Andy Groome <groome.andy@gmail.com>
2023-04-26 02:23:23 -0400

    initial commit

---
+++ tio-config/models/network.py
@@ -0,0 +1,20 @@
+
+from pydantic import Extra, Field, validator
+from typing import Optional
+from models.base_model import CustomBase
+
+class Network(CustomBase, extra=Extra.allow):
+    name: str = Field(alias='network_name')
+    description: Optional[str]
+    assets_ttl_days: Optional[int] 
+
+    @validator('assets_ttl_days')
+    def validate_ttl(cls, value):
+        if isinstance(value, int) and not 0 <= value <= 365:
+            raise ValueError(f'[{value}]: assets_ttl_days must be between 90 and 365')
+        return value
+        
+
+    def execute(self, tio):
+        network = tio.networks.create(**self.dict())
+        return network
