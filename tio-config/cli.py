f0baed9c9afdd873da909d1f81aad8ea25cf0efd
Andy Groome <groome.andy@gmail.com>
2023-04-26 01:05:05 -0400

    added key config

--- tio-config/cli.py
+++ tio-config/cli.py
@@ -1,8 +1,18 @@
 
 import click
+import click_pathlib
+import pathlib
 import workbook
 
+from tenable.io import TenableIO
+from dotenv import load_dotenv
+from pprint import pprint
+
 DEFAULT_CONFIG = './tio-config.xlsx'
+DEFAULT_ENV = './.env'
+
+load_dotenv()
+tio: TenableIO = TenableIO()
 
 @click.group()
 def cli():
@@ -10,9 +20,10 @@ def cli():
 
 @cli.command()
 @click.argument('sheet_name')
-@click.option('-c', '--config', type=click.Path(exists=True), default=DEFAULT_CONFIG)
+@click.option('-c', '--config', type=click_pathlib.Path(exists=True), default=DEFAULT_CONFIG)
 def load(sheet_name, config):
     '''load a single sheet from an excel file'''
+
     click.echo(f"loading {sheet_name} from {config}")
     sheets = workbook.WorkSheets(config)
     try:
@@ -22,9 +33,10 @@ def load(sheet_name, config):
 
 
 @cli.command()
-@click.option('-c', '--config', type=click.Path(exists=True), default=DEFAULT_CONFIG)
+@click.option('-c', '--config', type=click_pathlib.Path(exists=True), default=DEFAULT_CONFIG)
 def load_all(config):
     '''load all sheets from excel file'''
+
     sheets = workbook.Worksheets(config)
     for sheet_name in sheets.work_sheets:
         try:
@@ -32,5 +44,36 @@ def load_all(config):
         except workbook.SheetNotFound as e:
             print(repr(e))
 
+
+@cli.command()
+@click.option('-k', '--keyfile', type=click_pathlib.Path(), default=DEFAULT_ENV)
+def keys(keyfile: pathlib.Path):
+    '''update environment file with Tenable.io keys'''
+
+    if not keyfile.exists() or click.confirm('Overwrite existing file?'):
+        access_key = click.prompt('Tenable.io access_key')
+        secret_key = click.prompt('Tenable.io secret_key')
+        click.echo(f'writing keys to {keyfile}...')
+        with open(keyfile, 'w') as f:
+            f.write(f'TIO_ACCESS_KEY={access_key}\nTIO_SECRET_KEY={secret_key}')
+    else:
+        click.echo('operation canceled...')
+
+
+@cli.command()
+@click.argument('resource')
+@click.option('-f', '--fmt', type=str, help='python format string for output')
+def filters(resource, fmt):
+    click.echo(f'listing {resource} fmt="{fmt}"')
+    try:
+        api_method = getattr(tio.filters, resource)
+        print(f'api: {api_method}')
+    except TypeError:
+        click.BadParameter(f'[tio.filters.{resource}]: pyTenable api binding not found')
+    records = api_method()
+    pprint(records)
+
+    
+    
 if __name__ == '__main__':
     cli()
