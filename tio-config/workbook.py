f75a68d066fe2e953d817c0c2593435f4da7aa51
Andy Groome <groome.andy@gmail.com>
2023-04-30 16:17:22 -0400

    added Excel class

--- tio-config/workbook.py
+++ tio-config/workbook.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from dotenv import load_dotenv
+from typing import Optional
 from tenable.io import TenableIO
 
 from models.tag import Tag
@@ -15,7 +16,7 @@ class SheetNotFound(Exception):
     '''Raised when requested sheet is not in the workbook.'''
 
 
-model_classes = {
+data_models = {
     'agent_groups': AgentGroup,
     'exclusions': Exclusion,
     'networks': Network,
@@ -25,23 +26,58 @@ model_classes = {
 }
 
 
+class WorkSheet:
+    def __init__(self, excel: pd.ExcelFile, name: str) -> None: 
+        self.name = name
+        self.excel = excel
+        self.records = self.parse_records()
+
+    def parse_records(self):
+        df = self.excel.parse(self.name)
+        data = df.replace(np.nan, None).to_dict('records')
+
+        # the name of the sheet determines the data type
+        data_type = self.name
+        if data_type.startswith('tags') or data_type.endswith('tags'):
+            data_type = 'tags'
+        
+        cls = data_models.get(data_type)
+        if cls is None:
+            raise "unknown data model"
+
+        return [cls(**record) for record in data]
+
+    def dict(self):
+        return [record.dict() for record in self.records]
+
+
+class Excel:
+    def __init__(self, file_path: str) -> None:
+        self.excel = pd.ExcelFile(file_path)
+        self.work_sheets = {name: WorkSheet(self.excel, name) for name in self.excel.sheet_names}
+        self._file_path = file_path
+        self.sheet_names = self.excel.sheet_names
+
+    def get_objs(self, sheet_name: str, action=None):
+        objs = self.work_sheets[sheet_name].records
+        if action is not None:
+            for obj in objs:
+                obj.action = action
+        return objs
+
+    def dict(self):
+        return {k: v.dict() for k, v in self.work_sheets.items()}
+
+
+
 class WorkSheets:
 
-    def __init__(self, file_path: str, session: TenableIO = None) -> None:
+    def __init__(self, file_path: str, sheet_names: Optional[list[str]]=None) -> None:
         # read worksheets into a dictionary
         xl = pd.ExcelFile(file_path)
-        self._work_sheets = {sheet: xl.parse(sheet) for sheet in xl.sheet_names}
+        self._work_sheets = {s: xl.parse(s) for s in xl.sheet_names}
+        self.sheets = sheet_names
         self._file_path = file_path
-
-        # prepare a tio session if not passed in
-        self._session = session
-        if self._session is None: 
-            load_dotenv()
-            self._session = TenableIO()
-
-    @property
-    def sheets(self):
-        return list(self._work_sheets)
 
     def _load_sheet(self, sheet_name: str):
         df = self._work_sheets.get(sheet_name)
@@ -50,36 +86,31 @@ class WorkSheets:
 
         # clean DataFrame and convert to list of dicts
         data = df.replace(np.nan, None).to_dict('records')
+
+        # the name of the sheet determines the data type
+        data_type = sheet_name
+
+        # allow for more than one sheet of tags
+        if sheet_name.startswith('tags') or sheet_name.endswith('tags'):
+            data_type = 'tags'
         
-        # select data model based on the name of the sheet
         #  - expecting either a defined sheet name or starting/ending with 'tags'
-        cls = model_classes.get(sheet_name)
+        cls = data_models.get(sheet_name)
         if cls is None and sheet_name.startswith('tags') or sheet_name.endswith('tags'):
-            cls = model_classes.get('tags')
+            cls = data_models.get('tags')
 
         # convert to data model 
         if cls is not None:
             return [cls(**record) for record in data]
 
-    def execute(self, sheet_name: str):
-        commands = self._load_sheet(sheet_name)
-        print(f'executing sheet: {sheet_name}')
-        for command in commands:
-            print(command.describe)
-            command.execute(self._session)
+    def get_objs(self, sheet_name: str, action=None):
+        objs = self._load_sheet(sheet_name)
+        if action is not None:
+            for obj in objs:
+                obj.action = action
+        return objs
 
     def get_records(self, sheet_name: str):
         commands = self._load_sheet(sheet_name)
         return [command.dict() for command in commands]
-
-
-# cleanup function for testing
-def del_users(contains='+', dry_run=False):
-    load_dotenv()
-    tio = TenableIO()
-    for user in tio.users.list():
-        if contains in user['username']:
-            print(f"deleting: {user['username']}")
-            if not dry_run:
-                tio.users.delete(user['id'])
 
