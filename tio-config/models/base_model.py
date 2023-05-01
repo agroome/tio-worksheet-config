70710ebc5ed5027f3c21d2517b6bd1b0ec5856de
Andy Groome <groome.andy@gmail.com>
2023-04-30 22:05:15 -0400

    removed commened out

--- tio-config/models/base_model.py
+++ tio-config/models/base_model.py
@@ -8,13 +8,6 @@ class CustomBase(BaseModel, extra=Extra.allow, anystr_strip_whitespace=True):
 
     @root_validator(pre=True)
     def build_filters(cls, values: Dict[str, Any]) -> Dict[str, Any]:
-        '''Build the filter statement based on "extra fields" in values'''
-
-        '''
-        This function is run before field validation. Load self.filters using the input columns
-        that are NOT part of the class variables above.
-        '''
-
         def strip_lines(value):
             # replace newlines and remove spaces around commas
             if isinstance(value, str):
