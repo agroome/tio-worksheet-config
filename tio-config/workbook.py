dc62e13157a37b990815e4c0a980c0974db4c3f3
Andy Groome <groome.andy@gmail.com>
2023-05-13 13:04:27 -0400

    removed session dependency

--- tio-config/workbook.py
+++ tio-config/workbook.py
@@ -1,77 +1,211 @@
+import re
+import random
+import string
 import pandas as pd
 import numpy as np
-from dotenv import load_dotenv
+from collections import defaultdict
 from typing import Optional
-from tenable.io import TenableIO
 
-from command import BatchCommands
-from session import data_models
-
-from models.agent_group import AgentGroup
-from models.exclusion import Exclusion
-from models.group import Group
-from models.network import Network
-from models.scanner_group import ScannerGroup
-from models.tag import Tag
-from models.user import User
-
-# from processors import expand_users_with_groups
-
-class SheetNotFound(Exception):
-    '''Raised when requested sheet is not in the workbook.'''
-
-
-class WorkSheet:
-    def __init__(self, excel: pd.ExcelFile, name: str) -> None: 
-        self.name = name
-        self.excel = excel
-        self.records = self.parse_records()
-
-    def parse_records(self):
-        df = self.excel.parse(self.name)
-        data = df.replace(np.nan, None).to_dict('records')
-
-        # the name of the sheet determines the data type
-        data_type = self.name
-        if data_type.startswith('tags') or data_type.endswith('tags'):
-            data_type = 'tags'
-        
-        cls = data_models.get(data_type)
-        if cls is None:
-            raise Exception(f"[{data_type}]: unknown data model")
-
-        return [cls(**record) for record in data]
-
-    def dict(self):
-        return [record.dict() for record in self.records]
+PASSWORD_LENGTH = 20
 
 
 class Excel:
     def __init__(self, file_path: str, sheet_names: Optional[list]=None) -> None:
         self.excel = pd.ExcelFile(file_path)
         self._file_path = file_path
-        self.sheet_names = self.excel.sheet_names
+        # include all sheets or just ones that were specified in sheet_names 
         if sheet_names is not None:
-            self.sheet_names = [name for name in self.sheet_names if name in sheet_names]
-        self.work_sheets = {name: WorkSheet(self.excel, name) for name in self.sheet_names}
+            self.sheet_names = [name for name in self.excel.sheet_names if name in sheet_names]
+        else:
+            self.sheet_names = self.excel.sheet_names
 
-    def get_objs(self, sheet_name: str, action):
-        objs = self.work_sheets[sheet_name].records
-        # if sheet_name == 'users':
-        #     objs = [obj for obj in expand_users_with_groups(objs)]
-        print(f'get objs for {action}')
-        for obj in objs:
-            obj.action = action
-        return objs
+        self.sheets = {name: self.parse_sheet(name) for name in self.sheet_names}
 
-    def execute(self, sheet_names:list = None, action: str ='create', dry_run: bool = False):
-        if sheet_names is None:
-            sheet_names = self.sheet_names
-        for sheet in sheet_names:
-            print(f"get batch for {action}")
-            cmds = BatchCommands(self.get_objs(sheet, action), action)
-            cmds.execute(dry_run)
+    def get_sheet_action(self, sheet_name, action):
+        return [record for record in self.records[sheet_name] if record['action'] == 'action']
 
-    def dict(self):
-        return {k: v.dict() for k, v in self.work_sheets.items()}
+    def parse_sheet(self, sheet_name: str):
+        df = self.excel.parse(sheet_name)
+        df = df.applymap(strip_whitespace)
+        records = df.replace(np.nan, None).to_dict('records')
+        return records
 
+    def get_records(self, action='create', asset_tag_filters=None):
+        return post_process_sheets(self.sheets, asset_tag_filters=asset_tag_filters, action=action) 
+
+
+def strip_whitespace(value):
+    if isinstance(value, str):
+        value = re.sub('\n+', ',', value.strip())
+        value = re.sub('[ ]*,[ ]*', ',', value.strip())
+    return value
+
+def generate_password(length=PASSWORD_LENGTH):
+    special_chars = '`~!@#$%^&*()-_+='
+    password_chars = string.digits + string.ascii_letters + special_chars
+    return "".join(random.choices(password_chars, k=length))
+
+def insert_password(user):
+    user['password'] = generate_password()
+    return user
+
+def username_to_lower(user):
+    user['username'] = user['username'].lower()
+    return user
+
+def str_to_api_name(name):
+    if name.startswith('tags') or name.endswith('tags'):
+        name = 'tags'
+    return name
+
+
+def parse_filter_name(column_name: str, asset_tag_filters: dict) -> tuple[dict, str]:
+    '''split value into (filter_name, operator)
+    
+    The filter column name will be either:
+        - the name of a filter, i.e. ipv4
+        - OR the <name><' ' or '_'><operator
+
+    '''
+    # default to equal when the value is a filter_name with out the operator
+    operator = 'eq'
+    filter_name = column_name
+    
+    # get the dictionary of valid filter names and operators
+    tag_filter = asset_tag_filters.get(column_name)
+
+    if tag_filter is None:
+        # see if there is an operator appended to the filter_name
+        match = re.match('(?P<filter_name>\w+)[ _](?P<operator>\w+)', column_name)
+        if match is None:
+            raise ValueError(f'[{column_name}]: bad format')
+            
+        filter_name, operator = match.groups()
+        tag_filter = asset_tag_filters.get(filter_name)
+
+        if tag_filter is None:
+            raise KeyError(f'[{filter_name}]: filter name not found')
+    
+        if operator not in tag_filter['operators']:
+            raise KeyError(f'[{operator}]: not in {tag_filter["operators"]}')
+
+    return filter_name, operator
+
+
+def build_filters(columns, asset_tag_filters: dict):
+        '''Build the filter statement based on multiple columns.'''
+        header_fields=('category', 'value', 'filter_type')
+        # filter items are all k, v pairs execept 
+        filter_values = {k: v for k, v in columns.items() if k not in header_fields}
+        filters = []
+        for field_name, field_value in filter_values.items():
+            filter_name, operator = parse_filter_name(field_name, asset_tag_filters)
+            if field_value is None:
+                continue
+
+            field_value = re.sub('\n+', ',', field_value.strip())
+            field_value = re.sub('[ ]*,[ ]*', ',', field_value)
+
+            filters.append((filter_name, operator, field_value))
+                
+
+        # build a record with only the header file
+        record = {k: v for k, v in columns.items() if k in header_fields}
+        record['filters'] = filters
+
+        return record
+
+
+def process_groups_from_users(records):
+
+    membership = defaultdict(list)
+    group_commands = []
+    
+    for user in records:
+        if 'groups' not in user['record']:
+            continue
+            
+        # build group commands and membership index
+        for group_name in user['record']['groups'].split(','):
+            group_cmd = {
+                'api_name': 'groups',
+                'action': 'create',
+                'record': {
+                    'name': group_name
+                }
+            }  
+            membership[group_name].append(user['record']['username'])
+            group_commands.append(group_cmd)
+    
+    # build group 'add_user' from membership info
+    for group_name, members in membership.items():
+        usernames = sorted([u for u in members])
+        for name in usernames:
+            group_cmd = {
+                'api_name': 'groups',
+                'action': 'add_user',
+                'record': {
+                    'group_id': None,
+                    'user_id': None,
+                    'group_name': group_name,
+                    'username': name
+                }
+            }
+            group_commands.append(group_cmd)
+
+    return group_commands
+
+
+def post_process_sheets(sheets: dict[str,list], asset_tag_filters: dict = None, action: str = None):
+    '''Process sheets to do things build filters from columns and identify groups within user records
+    
+    Args:
+        sheets: lists of records for each sheet, indexed by sheet name
+        asset_tag_filters: valid tag filters from tio.filters.asset_tag_filters()
+            
+    '''
+
+    for name, records in sheets.items():
+        # we may have to convert the sheetname, i.e. tags_ipv4 or ipv4_tags to tags
+        api_name = str_to_api_name(name)
+        
+        # generate a password for each user record
+        if api_name == 'users':
+            records = map(insert_password, records)
+                              
+        # build filter parameters from columns
+        if api_name == 'tags' and asset_tag_filters is not None:
+            # records = map(build_filters, records)
+            records = [build_filters(record, asset_tag_filters) for record in records]
+            
+        def encapsulate(r):
+            return {'api_name': api_name, 'action': r.get('action', action), 'record': r}
+        
+        sheets[name] = [encapsulate(r) for r in records]
+    
+    # expand 'group create' and 'group add user' for groups defined in the user records 
+    if 'users' in sheets:
+        groups = process_groups_from_users(sheets['users'])
+        if 'groups' in sheets:
+            sheets['groups'].append(groups)
+        else:
+            sheets['groups'] = groups
+        
+    # combine multiple sheets of tags in to a single 'tags' entry
+    tag_sheets = {k: v for k, v in sheets.items() if k.startswith('tags') or k.endswith('tags')}
+    tag_records = [record for records in tag_sheets.values() for record in records]
+
+    # remove identified tag_sheets from sheets, add combined lists of records to sheets['tags']
+    for name in list(tag_sheets):
+        del sheets[name]
+    sheets['tags'] = tag_records
+    
+    return sheets
+
+
+def main():
+    ws = Excel('tio-config.xlsx')
+    _records = ws.get_records(encapsulate=False)
+
+if __name__ == '__main__':
+    main()
